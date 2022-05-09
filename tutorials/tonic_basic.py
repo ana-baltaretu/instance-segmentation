51d6afe37cae72ef2ecf3eccaeb4c2c11aea68e4
Ana Băltărețu <a.baltaretu@student.tudelft.nl>
2022-05-09 16:44:37 +0200

    Add pos-neg 3D plot generation + more repos

--- tutorials/tonic_basic.py
+++ tutorials/tonic_basic.py
@@ -3,7 +3,7 @@
 # https://github.com/neuromorphs/tonic
 # https://github.com/tihbe/python-ebdataset
 # https://github.com/TimoStoff/event_utils
-
+import numpy as np
 import tonic
 import tonic.transforms as transforms
 import matplotlib.pyplot as plt
@@ -62,13 +62,52 @@ def plot_time_surfaces():
 
     plt.show()
 
+
+def plot_1_channel_3D(x_data, y_data, z_data, cmap, save_path):
+    ax = plt.axes(projection='3d')
+
+    ax.scatter3D(z_data, x_data, y_data, c=z_data, cmap=cmap)
+    for ii in range(0, 360, 10):
+        ax.view_init(elev=0, azim=ii)
+        plt.savefig(save_path + "%d.png" % ii)
+    plt.show()
+
+
 if __name__ == '__main__':
     # plot_frames(frames)
 
-    # plot_frames_denoised()
+    plot_frames_denoised()
 
     # plot_voxel_grid()
-    print(events)
-    for event in events:
-        print(event)
+    # print(events)
+
+    x_data_pos = np.array([])   # X-axis
+    y_data_pos = np.array([])   # Y-axis
+    z_data_pos = np.array([])   # Time
+
+    x_data_neg = np.array([])   # X-axis
+    y_data_neg = np.array([])   # Y-axis
+    z_data_neg = np.array([])   # Time
+
+    time_pos = 0
+    time_neg = 0
+
+    div_rate = 100
+    mult_rate = 100
+
+    for (x, y, time, p) in events:
+        if p == 0:
+            x_data_neg = np.insert(x_data_neg, len(x_data_neg), x)
+            y_data_neg = np.insert(y_data_neg, len(y_data_neg), y)
+            z_data_neg = np.insert(z_data_neg, len(z_data_neg), int(time_neg/div_rate) * mult_rate)
+            time_neg += 1
+
+        else:
+            x_data_pos = np.insert(x_data_pos, len(x_data_pos), x)
+            y_data_pos = np.insert(y_data_pos, len(y_data_pos), y)
+            z_data_pos = np.insert(z_data_pos, len(z_data_pos), int(time_pos/div_rate) * mult_rate)
+            time_pos += 1
+
+    plot_1_channel_3D(x_data_pos, y_data_pos, z_data_pos, "Blues", "plots/pos")
+    plot_1_channel_3D(x_data_neg, y_data_neg, z_data_neg, "Reds", "plots/neg")
     # plot_time_surfaces()
